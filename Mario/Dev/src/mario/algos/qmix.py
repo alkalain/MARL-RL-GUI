d008bc230692b446a260f87dd4d9f009c3f2f6e8
vignee <vigne.enzo73@gmail.com>
2026-06-12 10:39:47 +0200

    refactor: changement de la structure de l'algo Q-mix pour correspondre à la généralisation de la méthode d'entrainement

---
+++ Mario/Dev/src/mario/algos/qmix.py
@@ -0,0 +1,25 @@
+from marllib import marl
+from mario.algos.base import Algo, ArchitectureSupport
+from mario.algos.architectures import MLPArchitecture
+
+class QMixAlgo(Algo):
+    """
+    Implémentation de l'algorithme Q-Mix s'appuyant sur MARLlib.
+    """
+    def __init__(
+            self,
+            architecture: ArchitectureSupport = None,
+            hyperparams: dict = None,
+            ):
+        super().__init__(
+            algo_type="QMix",
+            hyperparams=hyperparams or {
+                "lr": 0.0005,
+                "batch_size": 64, # Q-Mix a souvent des besoins en batch différents
+            }
+        )
+        self.architecture = architecture or MLPArchitecture()
+
+    def _get_marllib_algo(self, env_name: str):
+        return marl.algos.qmix(hyperparam_source=env_name)
+    
